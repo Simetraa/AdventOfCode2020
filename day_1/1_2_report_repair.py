6a9be6dcc06fc777363d953307a23023de5b9b4d
67224309+Simetraa@users.noreply.github.com <67224309+Simetraa@users.noreply.github.com>
2020-12-02 16:29:52 +0000

    Day 1 completed

---
+++ day_1/1_2_report_repair.py
@@ -0,0 +1,11 @@
+with open("1_report_repair_input.txt") as f:
+    expenses = f.read()
+
+expenses = expenses.split()
+expenses = [int(expense) for expense in expenses]
+
+for expense1 in expenses:
+    for expense2 in expenses:
+        for expense3 in expenses:
+            if expense1 + expense2 + expense3 == 2020:
+                print(expense1*expense2*expense3)
